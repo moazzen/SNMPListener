7cf4884ca293d185b44d81dc515cb8cb7e03dafd
Saeed Moazzen <saeed.moazzen@gmail.com>
2022-10-06 15:18:32 +0330

    New spring boot project

---
+++ src/main/java/com/mzn/snmp/SNMPListener/SnmpListenerApplication.java
@@ -0,0 +1,13 @@
+package com.mzn.snmp.SNMPListener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnmpListenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnmpListenerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mzn/snmp/SNMPListener/SnmpListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mzn.snmp.SNMPListener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnmpListenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
